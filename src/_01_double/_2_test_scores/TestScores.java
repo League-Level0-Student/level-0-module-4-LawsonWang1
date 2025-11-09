995563d0bf8e127d7cae23a4871cad407706e3fa
league <league@192.168.1.80>
2025-11-09 11:31:12 -0800

    Working on TestScores

--- src/_01_double/_2_test_scores/TestScores.java
+++ src/_01_double/_2_test_scores/TestScores.java
@@ -1,5 +1,14 @@
 package _01_double._2_test_scores;
 
+import javax.swing.JOptionPane;
+
 public class TestScores {
 
+	String score = JOptionPane.showInputDialog("What was your last test score?");
+	int scoreAsInt = Integer.parseInt(score);
+	if (scoreAsInt >= 90) {
+		
+	}
+	
+	
 }
